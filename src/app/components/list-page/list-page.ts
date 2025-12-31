e227db5f2fd3b02513944c39801b2dd6c0d15370
Alessandro Han <81700194+aleh02@users.noreply.github.com>
2025-12-31 17:39:59 +0100

    added open and back button

--- src/app/components/list-page/list-page.ts
+++ src/app/components/list-page/list-page.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
 import { List } from '../list/list';
 import { ListService } from '../../services/list.service';
 
 @Component({
   selector: 'app-list-page',
-  imports: [ FormsModule, RouterLink, List ],
+  imports: [ FormsModule, List ],
   templateUrl: './list-page.html',
   styleUrl: './list-page.css',
 })
